bd4d62a3093d2f3266769d4ee7f5bc546b5b182e
aloulou_dev <aloulou@bc.edu>
2025-10-27 14:18:34 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Malek Aloulou on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
